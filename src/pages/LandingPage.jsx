a444904520d9a916808507cc3e04375e72b4fd08
Reyhan Maulana <144551736+Rerey155-del@users.noreply.github.com>
2024-11-15 17:48:58 +0700

    siaaap

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -79,7 +79,7 @@ const LandingPage = () => {
                 Wadah pemberdayaan mahasiswa yang berfokus membangun
                 keterampilan dalam mengenal dunia teknologi
               </p>
-              <div className="flex space-x-4 justify-center md:justify-start text-white font-[Inter]">
+              <div className="flex space-x-4 mb-8 justify-center md:justify-start text-white font-[Inter]">
                 <button className="rounded-lg bg-[#F16634] p-3 font-semibold cursor-pointer transition duration-300 hover:scale-105 hover:shadow-lg">
                   Kenali Kami
                 </button>
@@ -96,7 +96,7 @@ const LandingPage = () => {
         </div>
 
         <div>
-          <div className="text-2xl justify-center text-center font-bold lg:text-2xl mb-10">
+          <div className="text-xl justify-center text-center font-bold lg:text-2xl mb-10">
             <h2>Unit Kegiatan Mahasiswa </h2>
             <h2>Dengan Divisi Standar Industri</h2>
           </div>
@@ -128,7 +128,7 @@ const LandingPage = () => {
         </div>
         <div className="mb-20 mt-28">
           <div>
-            <div className="justify-center text-center font-bold text-2xl mb-5">
+            <div className="justify-center text-xl text-center font-bold lg:text-2xl mb-5">
               <h2>Portofolio Kami</h2>
             </div>
             <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-4 p-8 mx-auto justify-items-center font-[Inter]">
@@ -171,7 +171,7 @@ const LandingPage = () => {
               Selengkapnya
             </button>
           </div>
-          <div className="text-2xl justify-center text-center font-bold lg:text-2xl mb-7 mt-[10rem]">
+          <div className="text-xl justify-center text-center font-bold lg:text-2xl mb-7 mt-[10rem]">
             <h2>Dedikasi Berbuah Cerita Yang Mengesankan</h2>
 
             <div className="grid gap-4 px-20 mt-10">
@@ -224,7 +224,7 @@ const LandingPage = () => {
           </div>
           <div className="px-2 lg:px-10 max-w-full mt-16">
             <div>
-              <h2 className="text-2xl text-center font-bold lg:text-2xl mb-5">
+              <h2 className="text-xl text-center font-bold lg:text-2xl mb-5">
                 Hilangkan Rasa Penasaran Dengan mengenal bagian dari kami
               </h2>
             </div>
