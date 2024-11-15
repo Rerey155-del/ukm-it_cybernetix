ef7b655b7be4be452453c4df968955a2d524d2be
Reyhan Maulana <144551736+Rerey155-del@users.noreply.github.com>
2024-11-15 10:02:19 +0700

    oke

--- src/pages/StrukturalPage.jsx
+++ src/pages/StrukturalPage.jsx
@@ -3,6 +3,7 @@ import glowEffect from "../assets/glow effect.svg";
 import Navbar from "../Components/Navbar";
 import together from "../assets/Together.jpg";
 import axios from "axios";
+import Sidebar from "../Components/Sidebar";
 
 const StrukturalPage = () => {
   const [users, setUsers] = useState([]);
@@ -52,56 +53,59 @@ const StrukturalPage = () => {
         className="text-black"
       >
         <Navbar />
-        <div className="font-[Lora] p-10 justify-center font-bold text-3xl mb-4 max-w-full mt-6">
-          <h2 data-aos="fade-up" className="text-center mb-10">
+        <Sidebar />
+        <div className="font-[Lora] p-4 sm:p-6 md:p-10 justify-center font-bold text-2xl md:text-3xl mb-4 max-w-full mt-6">
+          <h2 data-aos="fade-up" className="text-center mb-6 sm:mb-8 md:mb-10">
             Struktural 2024/2025
           </h2>
           <img
             data-aos="zoom-in"
-            className="rounded-2xl h-[28rem] mx-auto"
+            className="rounded-2xl w-full h-auto max-h-[40rem] sm:max-h-[28rem] md:max-h-[32rem] lg:max-h-[36rem] object-cover mx-auto"
             src={together}
             alt=""
           />
         </div>
 
-        <div className="px-10 max-w-full mt-16">
-          <h2 className="font-[Lora] font-semibold text-3xl mb-4">
+
+        <div className="px-2 lg:px-10 max-w-full mt-16">
+          <h2 className="pl-4 text-2xl font-[Lora] font-semibold lg:text-3xl mb-4">
             Struktural Inti
           </h2>
           <div
-            className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-6 md:gap-8 p-4 sm:p-6 md:p-8 lg:p-10 mx-auto justify-items-center font-[Inter] w-full"
-            style={{ overflow: "hidden" }} // Menghilangkan scroll di grid
+            className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 md:p-6 lg:p-8 mx-auto justify-items-center font-[Inter]"
+            style={{ overflow: "hidden" }}
           >
             {users.map((user, index) => (
               <div
                 key={index}
-                className="bg-white w-full sm:w-48 md:w-60 shadow-lg rounded-3xl p-4 sm:p-6 md:p-8"
+                className="bg-white card-body sm:w-[10rem] p-6 text-sm  md:w-60  lg:w-60 shadow-xl rounded-3xl"
                 data-aos="fade-up"
                 data-aos-duration="2000"
               >
                 <img
                   src={user.foto}
                   alt={user.nama}
-                  className="w-full h-auto rounded-t-3xl"
+                  className="w-full h-auto rounded-t-3xl object-cover"
                 />
                 <div className="text-center mt-3">
-                  <p className="font-bold">{user.nama}</p>
-                  <p>{user.jabatan}</p>
+                  <p className="font-bold text-base md:text-lg">{user.nama}</p>
+                  <p className="text-sm md:text-base">{user.jabatan}</p>
                 </div>
               </div>
             ))}
           </div>
-          <h2 className="font-[Lora] font-semibold text-3xl mb-4 mt-6">
+
+          <h2 className="pl-4 text-2xl font-[Lora] font-semibold lg:text-3xl mb-4 mt-4">
             Bidang Penelitian & Pengembangan
           </h2>
           <div
-            className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-6 md:gap-8 p-4 sm:p-6 md:p-8 lg:p-10 mx-auto justify-items-center font-[Inter] w-full"
+            className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 md:p-6 lg:p-8 mx-auto justify-items-center font-[Inter]"
             style={{ overflow: "hidden" }} // Menghilangkan scroll di grid
           >
             {litbangUser.map((user, index) => (
               <div
                 key={index}
-                className="bg-white w-full sm:w-48 md:w-60 shadow-lg rounded-3xl p-4 sm:p-6 md:p-8"
+                className="bg-white card-body sm:w-[10rem] p-6 text-sm  md:w-60  lg:w-60 shadow-xl rounded-3xl"
                 data-aos="fade-up"
                 data-aos-duration="2000"
               >
@@ -117,17 +121,17 @@ const StrukturalPage = () => {
               </div>
             ))}
           </div>
-          <h2 className="font-[Lora] font-semibold text-3xl mb-4 mt-6">
+          <h2 className="pl-4 text-2xl font-[Lora] font-semibold lg:text-3xl mb-4 mt-4">
             Bidang Informasi dan Komunikasi
           </h2>
           <div
-            className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-6 md:gap-8 p-4 sm:p-6 md:p-8 lg:p-10 mx-auto justify-items-center font-[Inter] w-full"
+            className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 md:p-6 lg:p-8 mx-auto justify-items-center font-[Inter]"
             style={{ overflow: "hidden" }} // Menghilangkan scroll di grid
           >
             {infokomUser.map((user, index) => (
               <div
                 key={index}
-                className="bg-white w-full sm:w-48 md:w-60 shadow-lg rounded-3xl p-4 sm:p-6 md:p-8"
+                className="bg-white card-body sm:w-[10rem] p-6 text-sm  md:w-60  lg:w-60 shadow-xl rounded-3xl"
                 data-aos="fade-up"
                 data-aos-duration="2000"
               >
@@ -143,17 +147,17 @@ const StrukturalPage = () => {
               </div>
             ))}
           </div>
-          <h2 className="font-[Lora] font-semibold text-3xl mb-4 mt-6">
+          <h2 className="pl-4 text-2xl font-[Lora] font-semibold lg:text-3xl mb-4 mt-4">
             Bidang Hubungan Masyarakat
           </h2>
           <div
-            className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-6 md:gap-8 p-4 sm:p-6 md:p-8 lg:p-10 mx-auto justify-items-center font-[Inter] w-full"
+            className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 md:p-6 lg:p-8 mx-auto justify-items-center font-[Inter]"
             style={{ overflow: "hidden" }} // Menghilangkan scroll di grid
           >
             {humasUser.map((user, index) => (
               <div
                 key={index}
-                className="bg-white w-full sm:w-48 md:w-60 shadow-lg rounded-3xl p-4 sm:p-6 md:p-8"
+                className="bg-white card-body sm:w-[10rem] p-6 text-sm  md:w-60  lg:w-60 shadow-xl rounded-3xl"
                 data-aos="fade-up"
                 data-aos-duration="2000"
               >
@@ -169,17 +173,17 @@ const StrukturalPage = () => {
               </div>
             ))}
           </div>
-          <h2 className="font-[Lora] font-semibold text-3xl mb-4 mt-6">
+          <h2 className="pl-4 text-2xl font-[Lora] font-semibold lg:text-3xl mb-4 mt-4">
             Bidang Pengembangan Sumber Daya Manusia
           </h2>
           <div
-            className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-6 md:gap-8 p-4 sm:p-6 md:p-8 lg:p-10 mx-auto justify-items-center font-[Inter] w-full"
+            className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 md:p-6 lg:p-8 mx-auto justify-items-center font-[Inter]"
             style={{ overflow: "hidden" }} // Menghilangkan scroll di grid
           >
             {psdmUser.map((user, index) => (
               <div
                 key={index}
-                className="bg-white w-full sm:w-48 md:w-60 shadow-lg rounded-3xl p-4 sm:p-6 md:p-8"
+                className="bg-white card-body sm:w-[10rem] p-6 text-sm  md:w-60  lg:w-60 shadow-xl rounded-3xl"
                 data-aos="fade-up"
                 data-aos-duration="2000"
               >
