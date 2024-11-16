f5d026ad8ed03983409300df123de044d011ec30
Agil-Saputra <asagil67@gmail.com>
2024-11-16 15:10:39 +0700

    baru Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,25 +2,25 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-gradient-to-r from-blue-900 to-orange-500 text-white py-12">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-start space-y-10 md:space-y-0 px-6">
-        {/* Bagian Kiri */}
-        <div className="max-w-xs">
+    <footer className="bg-gradient-to-r from-blue-900 to-orange-500 text-white opacity-80 py-12">
+      <div className="container mx-auto flex flex-col lg:flex-row justify-between items-start space-y-10 lg:space-y-0 px-6">
+        {/* Bagian Kiri Lebih Lebar */}
+        <div className="col-span-2 flex flex-col items-center justify-center max-w-lg mx-auto lg:mx-0">
           <img src="/src/assets/cx-logo.svg" alt="Logo" className="w-20 mb-4" />
-          <h2 className="text-lg font-semibold">
+          <h2 className="text-lg font-semibold text-center">
             Unit Kegiatan Mahasiswa
             <br />
             Information Technology
             <br />
             Cybernetix
           </h2>
-          <p className="mt-4">
+          <p className="mt-4 text-center">
             &copy; 2007-2023 <strong>WebDev</strong> - UKM IT Cybernetix
           </p>
         </div>
 
         {/* Bagian Kolom */}
-        <div className="grid grid-cols-2 md:grid-cols-5 gap-8">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 flex-grow">
           {/* Divisi */}
           <div>
             <h3 className="font-semibold mb-4">Divisi</h3>
