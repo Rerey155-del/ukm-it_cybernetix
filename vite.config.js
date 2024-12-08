589a863e86a61bcbef19608572c244effecdb064
Reyhan Maulana <144551736+Rerey155-del@users.noreply.github.com>
2024-12-08 11:34:49 +0700

    pembaharuan

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    sourcemap: false, // Matikan source map di produksi
+    minify: 'terser',
+    terserOptions: {
+      mangle: true, // Ubah nama variabel agar lebih sulit dibaca
+      compress: true, // Optimasi kompresi
+    }, 
+  },
+});
