24602fd7667f8778444cd6bc6dd2035e324de6a0
Reyhan Maulana <144551736+Rerey155-del@users.noreply.github.com>
2024-12-09 23:53:32 +0700

    pemasangan IP Publict pada web

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '165.22.99.299', 
+    port: 3000       
+  },
   plugins: [react()],
   build: {
     sourcemap: false, // Matikan source map di produksi
