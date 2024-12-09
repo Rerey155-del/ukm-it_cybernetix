7b5321450267b558ea5fe7a61aa8970e8dc3880e
Reyvars <144551736+Rerey155-del@users.noreply.github.com>
2024-12-09 23:56:49 +0700

    ok

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   server: {
     host: '165.22.99.299', 
-    port: 3000       
+    port: 343434      
   },
   plugins: [react()],
   build: {
