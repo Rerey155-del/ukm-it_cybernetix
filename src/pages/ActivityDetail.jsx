8189a702a67fb5d32e4dcca7c2c6e837de57b4d3
Reyhan Maulana <144551736+Rerey155-del@users.noreply.github.com>
2024-12-16 17:49:09 +0700

    pembaharuan

--- src/pages/ActivityDetail.jsx
+++ src/pages/ActivityDetail.jsx
@@ -17,25 +17,15 @@ const ActivityDetail = () => {
   };
 
   useEffect(() => {
-    // Menggunakan id untuk fetch data dari backend
     axios
-      .get(`https://express-mongo-lac.vercel.app/articles/${id}`) // Endpoint backend untuk mengambil artikel berdasarkan id
+      .get(`https://express-mongo-lac.vercel.app/articles/${id}`)
       .then((response) => {
-        setArtikel(response.data); // Set data yang diterima ke state artikel
+        setArtikel(response.data);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
-  }, [id]); // Efek dijalankan setiap kali id berubah
-
-  // Pastikan artikel ada sebelum mencoba merender
-  if (!artikel) {
-    return (
-      <div className="skeleton w-full h-full bg-gray-300 animate-pulse">
-
-      </div>
-    );
-  }
+  }, [id]);
 
   return (
     <section>
@@ -60,82 +50,98 @@ const ActivityDetail = () => {
         <Navbar darkMode={darkModes} toggleDarkMode={toggleDarkMode} />
         <Sidebar darkMode={darkModes} toggleDarkMode={toggleDarkMode} />
         <div className="flex flex-col gap-6">
-          <img className="w-full h-full" src={artikel.gambar} alt={artikel.nama} data-aos="fade-down" data-aos-duration="1500" />
-          <div className="flex mt-4 container mx-auto" data-aos="fade-up">
-            <div className=" p-5">
-              <h2 className="font-bold text-2xl">{artikel.nama}</h2>
-              <div className="flex gap-5 mt-5 mb-5">
-                <p>UKM-IT Cybernetix</p>
-                <p>11 Desember 2023</p>
-              </div>
-              <p>{artikel.isi}</p>
+          {/* Kondisi jika artikel masih null */}
+          {!artikel ? (
+            <div className="skeleton mx auto bg-transparent mb-6 mt-16 p-6 flex flex-col lg:flex-row items-center gap-6 lg:gap-8 w-full h-[35rem] animate-pulse"/>
+          ) : (
+            <>
+              <img
+                className="w-full h-full"
+                src={artikel.gambar}
+                alt={artikel.nama}
+                data-aos="fade-down"
+                data-aos-duration="1500"
+              />
+              <div
+                className="flex mt-4 container mx-auto"
+                data-aos="fade-up"
+              >
+                <div className="p-5">
+                  <h2 className="font-bold text-2xl">{artikel.nama}</h2>
+                  <div className="flex gap-5 mt-5 mb-5">
+                    <p>UKM-IT Cybernetix</p>
+                    <p>11 Desember 2023</p>
+                  </div>
+                  <p>{artikel.isi}</p>
 
-              <div className="flex gap-10 mt-6 justify-center">
-                <div className={`card ${darkModes ? "bg-[#32364F]" : "bg-white"} h-full shadow-xl w-[25rem] gap-5 p-10 items-center`}>
-                  <p className="font-bold ">Apa kata peserta?</p>
-                  {artikel.reviewPeserta?.map((review, index) => (
+                  <div className="flex gap-10 mt-6 justify-center">
+                    {/* Kartu Review Peserta */}
                     <div
-                      key={index}
-                      className={`p-4 card shadow-xl w-[20rem] ${darkModes ? "bg-[#383E5E]" : "bg-white"
-                        }`}
+                      className={`card ${
+                        darkModes ? "bg-[#32364F]" : "bg-white"
+                      } h-full shadow-xl w-[25rem] gap-5 p-10 items-center`}
                     >
-                      <div className="flex items-center gap-5">
-                        {artikel.fotoReviewPeserta && artikel.fotoReviewPeserta[index] && (
-                          <img
-                            src={artikel.fotoReviewPeserta[index]}
-                            
-                            className="w-12 h-12 object-cover rounded-full items-center" // Ukuran XS dengan gaya lingkaran
-                          />
-                        )}
-                        <p>{review}</p>
-                      </div>
+                      <p className="font-bold ">Apa kata peserta?</p>
+                      {artikel.reviewPeserta?.map((review, index) => (
+                        <div
+                          key={index}
+                          className={`p-4 card shadow-xl w-[20rem] ${
+                            darkModes ? "bg-[#383E5E]" : "bg-white"
+                          }`}
+                        >
+                          <div className="flex items-center gap-5">
+                            {artikel.fotoReviewPeserta &&
+                              artikel.fotoReviewPeserta[index] && (
+                                <img
+                                  src={artikel.fotoReviewPeserta[index]}
+                                  className="w-12 h-12 object-cover rounded-full items-center"
+                                />
+                              )}
+                            <p>{review}</p>
+                          </div>
+                        </div>
+                      ))}
                     </div>
-                  ))}
-
-                </div>
-                <div className={`card ${darkModes ? "bg-[#32364F]" : "bg-white"} shadow-xl w-[25rem] gap-5 h-full p-10 items-center`}>
-                  <p className="font-bold">Apa kata CX?</p>
-                  {artikel.reviewCX?.map((review, index) => (
+                    {/* Kartu Review CX */}
                     <div
-                      key={index}
-                      className={`p-4 card shadow-xl w-[20rem] ${darkModes ? "bg-[#383E5E]" : "bg-white"
-                        }`}
+                      className={`card ${
+                        darkModes ? "bg-[#32364F]" : "bg-white"
+                      } shadow-xl w-[25rem] gap-5 h-full p-10 items-center`}
                     >
-                      <div className="flex items-center gap-5">
-                        {artikel.fotoReviewCX && artikel.fotoReviewCX[index] && (
-                          <img
-                            src={artikel.fotoReviewCX[index]}
-                            
-                            className="w-12 h-12 object-cover rounded-full items-center" // Ukuran XS dengan gaya lingkaran
-                          />
-                        )}
-                        <p>{review}</p>
-                      </div>
+                      <p className="font-bold">Apa kata CX?</p>
+                      {artikel.reviewCX?.map((review, index) => (
+                        <div
+                          key={index}
+                          className={`p-4 card shadow-xl w-[20rem] ${
+                            darkModes ? "bg-[#383E5E]" : "bg-white"
+                          }`}
+                        >
+                          <div className="flex items-center gap-5">
+                            {artikel.fotoReviewCX &&
+                              artikel.fotoReviewCX[index] && (
+                                <img
+                                  src={artikel.fotoReviewCX[index]}
+                                  className="w-12 h-12 object-cover rounded-full items-center"
+                                />
+                              )}
+                            <p>{review}</p>
+                          </div>
+                        </div>
+                      ))}
                     </div>
-                  ))}
-
+                  </div>
                 </div>
-
-              </div>
-            </div>
-            <div className={`card ${darkModes ? "bg-[#32364F]" : "bg-white"} h-full shadow-xl w-[25rem] gap-5 p-10 items-center`}>
-              <p className=" font-bold ">Highlight Activity</p>
-
-              {artikel.reviewCX?.map((review, index) => (
                 <div
-                  key={index}
-                  className={`p-4 card shadow-xl w-[20rem] ${darkModes ? "bg-[#383E5E]" : "bg-white"
-                    }`}
+                  className={`card ${
+                    darkModes ? "bg-[#32364F]" : "bg-white"
+                  } h-full shadow-xl w-[25rem] gap-5 p-10 items-center`}
                 >
-                  
+                  <p className=" font-bold ">Highlight Activity</p>
                 </div>
-              ))}
-
-
-            </div>
-          </div>
+              </div>
+            </>
+          )}
         </div>
-
         <Footer darkMode={darkModes} />
       </div>
     </section>
