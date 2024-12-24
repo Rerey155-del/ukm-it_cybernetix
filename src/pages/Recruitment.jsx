40936919c31dc6c7291bd9cbb759f84453c43811
Reyhan Maulana <144551736+Rerey155-del@users.noreply.github.com>
2024-12-24 11:10:27 +0700

    oke

--- src/pages/Recruitment.jsx
+++ src/pages/Recruitment.jsx
@@ -91,7 +91,7 @@ const Recruitment = () => {
                                 <img
                                     src={fotolagi}
                                     alt=""
-                                    className=" w-[250px] h-full md:w-[450px] md:h-full lg:w-[450px] lg:h-full max-w-full"
+                                    className=" w-[450px] h-full md:w-[450px] md:h-full lg:w-[450px] lg:h-full max-w-full"
                                 />
                             </div>
 
