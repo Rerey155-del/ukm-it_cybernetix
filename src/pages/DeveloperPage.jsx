c46fd55c9faf200d80387f0eb42c1b4bf8c003c2
Reyhan Maulana <144551736+Rerey155-del@users.noreply.github.com>
2024-12-25 11:45:16 +0700

    oke

--- src/pages/DeveloperPage.jsx
+++ src/pages/DeveloperPage.jsx
@@ -3,7 +3,7 @@ import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import Rerey from "../assets/Rereydev.png";
+import Rerey from "../assets/rereydev.png";
 import Figma from "../assets/figma.png";
 import Reactjs from "../assets/reactjs.png";
 import Tailwind from "../assets/tailwind.png";
@@ -123,7 +123,6 @@ const CXDevelopers = () => {
             />
           </div>
         </div>
-
         {/* Developers Section */}
         <div className="container mb-6 mx-auto p-6 md:p-20 grid grid-cols-2 gap-x-4 gap-y-4 sm:grid-cols-2 lg:grid-cols-4" data-aos="fade-up" data-aos-duration="1500">
           {developers.slice(1).map((dev, index) => (
