006f12e367f155fe44326335c4999794f2fd9dbd
Reyhan Maulana <144551736+Rerey155-del@users.noreply.github.com>
2024-12-25 11:19:58 +0700

    oke

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,12 @@ export default defineConfig({
   //   host: '188.166.227.167', 
   //   port: 3500       
   // },
+  base: '/', // Path base untuk deployment
   plugins: [react()],
   build: {
     sourcemap: false, // Matikan source map di produksi
     minify: 'terser',
+    outDir: 'dist', // Folder output build
     terserOptions: {
       mangle: true, // Ubah nama variabel agar lebih sulit dibaca
       compress: true, // Optimasi kompresi
